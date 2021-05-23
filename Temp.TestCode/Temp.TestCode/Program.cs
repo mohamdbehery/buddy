a8099a1302fe4ac9097f85d13fa6d5f0344a3f38
Mohamed Behery <mohamd.behery@yahoo.com>
2021-05-23 20:55:49 +0200

    kjkjj

--- Temp.TestCode/Temp.TestCode/Program.cs
+++ Temp.TestCode/Temp.TestCode/Program.cs
@@ -15,29 +15,60 @@ namespace Temp.TestCode
         static Helper helper = Helper.CreateInstance();
         static void Main(string[] args)
         {
-            LinkedTest<string> linkedTest = new LinkedTest<string>();
-            linkedTest.ListCount
-            using (UnitOfWork unitOfWork = new UnitOfWork())
-            {
-                unitOfWork.AppUserRepository.AddUser(new App.Data.LogicalModelsDTO.AppUserModel()
-                {
-                    eMailAddress = "mohamd.behery.s@gmail.com",
-                    EntryDate = DateTime.Now,
-                    FamilyName = "Behery",
-                    FirstName = "Mohamed",
-                    IsActive = true,
-                    IsDeleted = false,
-                    LocationAddress = "Calle Moreti 9",
-                    ModifyDate = DateTime.Now,
-                    Password = "1234@$",
-                    SecondName = "Sabbah",
-                    UserName = "MBehery"
-                });
-                var affectedRowsCount = unitOfWork.Save();
-                Console.WriteLine(affectedRowsCount);
-            }
+            //LinkedTest<string> linkedTest = new LinkedTest<string>();
+            //linkedTest.ListCount
+            //using (UnitOfWork unitOfWork = new UnitOfWork())
+            //{
+            //    unitOfWork.AppUserRepository.AddUser(new App.Data.LogicalModelsDTO.AppUserModel()
+            //    {
+            //        eMailAddress = "mohamd.behery.s@gmail.com",
+            //        EntryDate = DateTime.Now,
+            //        FamilyName = "Behery",
+            //        FirstName = "Mohamed",
+            //        IsActive = true,
+            //        IsDeleted = false,
+            //        LocationAddress = "Calle Moreti 9",
+            //        ModifyDate = DateTime.Now,
+            //        Password = "1234@$",
+            //        SecondName = "Sabbah",
+            //        UserName = "MBehery"
+            //    });
+            //    var affectedRowsCount = unitOfWork.Save();
+            //}
+            var x = BinarySearch(new int[] { 1, 3, 5, 7, 10, 12 }, 7);
+            Console.WriteLine(x);
             Console.ReadLine();
         }
+
+        public int Factorial(int n)
+        {
+            if (n >= 1) { return n * Factorial(n - 1); }
+            else { return 1; }
+        }
+
+        public int Fibonacci(int n)
+        {
+            if (n >= 3) { return Fibonacci(n - 1) + Fibonacci(n + 2); }
+            else { return 1; }
+        }
+
+        public static int BinarySearch(int[] source, int target)
+        {
+            int left = 0, middle = 0;
+            int right = source.Length - 1;
+            while (left < right)
+            {
+                middle = (left + right) / 2;
+                if (source[middle] == target)
+                    return middle;
+                else if (target < source[middle])
+                    right = middle - 1;
+                else if (target > source[middle])
+                    left = middle + 1;
+            }
+            return middle;
+        }
+
         private static void writeTest(Phone v)
         {
             v.PhoneNumber = "ch";
