97b687125bb5868e7da296dea4cdbd3fe294c7ae
Mohamed Behery <mohamd.behery@yahoo.com>
2021-05-25 18:07:14 +0200

    asdf

--- Demo.Algorithms/Program.cs
+++ Demo.Algorithms/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Demo.Algorithms
 {
@@ -6,7 +7,17 @@ namespace Demo.Algorithms
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //int[] given_array = new[] { 1, 4, 14, 18, 3, 2 };
+            //var sorted_array = QuickSort(given_array, 0, given_array.Length);
+            //foreach (var item in sorted_array)
+            //{
+            //    Console.Write($" > {item}");
+            //}
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+            dict.Add("test", "y");
+            string d = "zsdf";
+            Console.WriteLine(d.GetHashCode());
+            Console.ReadLine();
         }
 
         public int Factorial(int n)
@@ -37,5 +48,42 @@ namespace Demo.Algorithms
             }
             return middle;
         }
+
+        # region Quick Sort
+        public static int[] QuickSort(int[] given_array, int start, int end)
+        {
+            if (start < end)
+            {
+                int pivotIndex = QuickSortPartition(given_array, start, end);
+                QuickSort(given_array, start, pivotIndex);
+                QuickSort(given_array, pivotIndex + 1, end);
+            }
+            return given_array;
+        }
+
+        public static int QuickSortPartition(int[] given_array, int left, int right)
+        {
+            int pivot = given_array[left];
+            int swapIndex = left;
+            for (int i = left + 1; i < right; i++)
+            {
+                if (given_array[i] < pivot)
+                {
+                    swapIndex++;
+                    SwapArray(ref given_array, i, swapIndex); // sort these 2 elements
+                }
+            }
+            SwapArray(ref given_array, left, swapIndex); // move pivot to its sorted location
+            return swapIndex;
+        }
+
+        public static void SwapArray(ref int[] given_array, int firstIndex, int secondIndex)
+        {
+            int temp = given_array[firstIndex];
+            given_array[firstIndex] = given_array[secondIndex];
+            given_array[secondIndex] = temp;
+        }
+        #endregion
+
     }
 }
