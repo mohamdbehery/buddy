61323c29646828b1a1c966b683ad2d805eec869d
Mohamed Behery <mohamd.behery@yahoo.com>
2021-06-08 13:07:47 +0200

    sdf

--- Demo.Algorithms/Program.cs
+++ Demo.Algorithms/Program.cs
@@ -7,12 +7,22 @@ namespace Demo.Algorithms
     {
         static void Main(string[] args)
         {
+            #region quick sort
             //int[] given_array = new[] { 1, 4, 14, 18, 3, 2 };
-            //var sorted_array = QuickSort(given_array, 0, given_array.Length);
+            //int[] sorted_array = QuickSort(given_array, 0, given_array.Length);
             //foreach (var item in sorted_array)
             //{
             //    Console.Write($" > {item}");
             //}
+            #endregion
+            #region merge sort
+            //int[] given_array = new[] { 1, 4, 14, 18, 3, 2 };
+            //int[] sorted_array = MergeSort(given_array, 0, given_array.Length - 1);
+            //foreach (var item in sorted_array)
+            //{
+            //    Console.Write($" > {item}");
+            //}
+            #endregion
             Console.WriteLine();
             Console.ReadLine();
         }
@@ -46,7 +56,7 @@ namespace Demo.Algorithms
             return middle;
         }
 
-        # region Quick Sort
+        #region Quick Sort
         public static int[] QuickSort(int[] given_array, int start, int end)
         {
             if (start < end)
@@ -82,5 +92,72 @@ namespace Demo.Algorithms
         }
         #endregion
 
+        #region Merge Sort
+        public static void Merge(int[] arr, int left, int middle, int right)
+        {
+            int leftArrayLength = middle - left + 1;
+            int rightArrayLength = right - middle;
+
+            // Devide array 
+            int[] leftArray = new int[leftArrayLength], rightArray = new int[rightArrayLength];
+            for (int x = 0; x < leftArrayLength; x++)
+                leftArray[x] = arr[left + x];
+            for (int y = 0; y < rightArrayLength; y++)
+                rightArray[y] = arr[middle + 1 + y];
+
+            // Maintain current index of sub-arrays and main array
+            int i, j, k;
+            i = 0;
+            j = 0;
+            k = left;
+
+            // Until we reach either end of either L or M, pick larger among
+            // elements L and M and place them in the correct position at A[p..r]
+            while (i < leftArrayLength && j < rightArrayLength)
+            {
+                if (leftArray[i] <= rightArray[j])
+                {
+                    arr[k] = leftArray[i];
+                    i++;
+                }
+                else
+                {
+                    arr[k] = rightArray[j];
+                    j++;
+                }
+                k++;
+            }
+
+            // When we run out of elements in either L or M,
+            // pick up the remaining elements and put in A[p..r]
+            while (i < leftArrayLength)
+            {
+                arr[k] = leftArray[i];
+                i++;
+                k++;
+            }
+
+            while (j < rightArrayLength)
+            {
+                arr[k] = rightArray[j];
+                j++;
+                k++;
+            }
+        }
+
+        public static int[] MergeSort(int[] given_array, int left, int right)
+        {
+            if (left >= right)
+            {
+                return given_array;
+            }
+            int middle = left + (right - left) / 2;
+            MergeSort(given_array, left, middle);
+            MergeSort(given_array, middle + 1, right);
+            Merge(given_array, left, middle, right); // merge two subarrays L and M into arr
+
+            return given_array;
+        }
+        #endregion
     }
 }
