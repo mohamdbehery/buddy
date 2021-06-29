1943b384b96eff73ee935f50e118d1bd6aee47b5
Mohamed Behery <mohamd.behery@yahoo.com>
2021-06-29 16:12:22 +0200

    dgfg

---
+++ Demo.DesignPatterns/Patterns/Proxy.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Patterns
+{
+    public class Proxy
+    {
+        public void ConsumeThePattern()
+        {
+            var proxy = new OurProxy();
+            proxy.Operation();
+        }
+    }
+
+    public abstract class SubjectBase
+    {
+        public abstract void Operation();
+    }
+
+    public class RealSubject : SubjectBase
+    {
+        public override void Operation()
+        {
+            Console.WriteLine("RealSubject.Operation");
+        }
+    }
+
+    public class OurProxy : SubjectBase
+    {
+        private RealSubject _realSubject;
+
+        public override void Operation()
+        {
+            _realSubject = _realSubject == null ? new RealSubject() : _realSubject;
+            _realSubject.Operation();
+        }
+    }
+}
