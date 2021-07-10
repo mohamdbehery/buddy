3e3799babdcf874ab2f40a120a523e486d51accf
Mohamed Behery <mohamd.behery@yahoo.com>
2021-07-10 20:19:53 +0200

    dfgdf

--- Demo.Tips/js/page.js
+++ Demo.Tips/js/page.js
@@ -5,12 +5,31 @@
         dataType: "xml",
         success: function (xml) {
             debugger;
-            if (xml.children[0] && xml.children[0].childNodes) {
-                var filledItems;
+            if (xml.children[0]) {
+                var sectionTemplate = $('#section-template').html();
                 var itemTemplate = $('#item-template').html();
-                for (let i = 0; i < xml.children[0].childNodes.length; i++) {
+
+                var filledSections = '';
+                for (let i = 0; i < xml.children[0].getElementsByTagName('Section').length; i++) {
+                    var section = xml.children[0].getElementsByTagName('Section')[i];
+                    var sectionTitle = section.attributes['Title'].value;
+                    var tempSectionTemplate = sectionTemplate.split('#SectionTitle#').join(sectionTitle);
                     
+                    var filledItems = '';
+                    for (let j = 0; j < section.getElementsByTagName('Item').length; j++) {
+                        var item = section.getElementsByTagName('Item')[j];
+                        var title = item.getElementsByTagName('Title')[0];
+                        var data = item.getElementsByTagName('Data')[0];
+                        var tempItemTemplate = itemTemplate.split('#ItemTitle#').join(title.innerHTML);
+                        tempItemTemplate = tempItemTemplate.split('#ItemData#').join(data.innerHTML);
+                        tempItemTemplate = tempItemTemplate.split('#ItemCode#').join('Item' + i + '_' + j);
+                        filledItems += tempItemTemplate;
+                    }
+                    tempSectionTemplate = tempSectionTemplate.split('#Items#').join(filledItems);
+                    filledSections += tempSectionTemplate;
                 }
+                debugger;
+                $('#sections').append(filledSections);
             }
         }
     });
