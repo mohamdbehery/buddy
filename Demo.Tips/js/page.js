3dc6e8fffd07bf8922cd7d3e4c9756e65b4cca5f
Mohamed Behery <mohamd.behery@yahoo.com>
2021-07-10 16:11:01 +0200

    cfgdfg

---
+++ Demo.Tips/js/page.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    $.ajax({
+        type: "GET",
+        url: "/review-source.xml",
+        dataType: "xml",
+        success: function (xml) {
+            debugger;
+            if (xml.children[0] && xml.children[0].childNodes) {
+                var filledItems;
+                var itemTemplate = $('#item-template').html();
+                for (let i = 0; i < xml.children[0].childNodes.length; i++) {
+                    
+                }
+            }
+        }
+    });
+});
